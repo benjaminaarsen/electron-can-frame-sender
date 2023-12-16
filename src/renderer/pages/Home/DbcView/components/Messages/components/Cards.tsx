636065f0399760b45014bcfe6f30e28752ed1c5f
benjaminaarsen <benjaminaarsen@outlook.com>
2023-12-16 16:18:35 +0100

    Refactor signal input components and add message data storage

--- src/renderer/pages/Home/DbcView/components/Messages/components/Cards.tsx
+++ src/renderer/pages/Home/DbcView/components/Messages/components/Cards.tsx
@@ -1,11 +1,13 @@
-import { Fragment, JSX, useState } from 'react';
+import React, { Fragment, JSX, useState } from 'react';
 import { Message, Signal } from 'dbc-can/lib/dbc/Dbc';
 import { Card, Form } from 'react-bootstrap';
+import { messageDataStore } from '../../../MessageData';
 
-function InputRange({ signal }: { signal: Signal }) {
+function InputRange({ message, signal }: { message: Message; signal: Signal }) {
   const [rangeValue, setRangeValue] = useState(0);
   const handleChange = (e) => {
     setRangeValue(e.target.value);
+    messageDataStore.get(message.id)?.set(signal.name, e.target.value);
   };
   return (
     <>
@@ -16,11 +18,109 @@ function InputRange({ signal }: { signal: Signal }) {
       <Form.Range
         key={`${signal.name} range`}
         value={rangeValue}
-        min={0}
-        max={2 ** signal.length - 1}
+        min={signal.signed ? 2 ** signal.length / -2 : 0}
+        max={
+          signal.signed ? (2 ** signal.length - 1) / 2 : 2 ** signal.length - 1
+        }
         onChange={handleChange}
       />
     </>
+  );
+}
+
+const oneBitSignal = ({
+  message,
+  signal,
+}: {
+  message: Message;
+  signal: Signal;
+}) => {
+  return (
+    <span className="d-flex justify-content-between">
+      <Form.Label className="text-wrap w-90">{`${signal.name}`}</Form.Label>
+      <Form.Check
+        onChange={(e) => {
+          messageDataStore
+            .get(message.id)
+            ?.set(signal.name, e.target.checked ? 1 : 0);
+          console.log(messageDataStore.get(message.id)?.get(signal.name));
+        }}
+        type="checkbox"
+      />
+    </span>
+  );
+};
+
+const twoBitSignal = ({
+  message,
+  signal,
+}: {
+  message: Message;
+  signal: Signal;
+}) => {
+  return (
+    <>
+      <span className="d-flex justify-content-between">
+        <Form.Label className="text-wrap w-100">{`${signal.name}`}</Form.Label>
+      </span>
+      <Form className="d-flex justify-content-between">
+        {[0, 1, 2, 3].map((i) => {
+          return (
+            <Form.Check
+              key={`${signal.name} ${i}`}
+              inline
+              label={`${i}`}
+              name={signal.name}
+              type="radio"
+              id={`inline-radio-${i}`}
+              value={i}
+              onChange={(e) => {
+                messageDataStore
+                  .get(message.id)
+                  ?.set(signal.name, parseInt(e.target.value, 10));
+                console.log(messageDataStore.get(message.id)?.get(signal.name));
+              }}
+            />
+          );
+        })}
+      </Form>
+    </>
+  );
+};
+
+function SignalsCard({
+  message,
+  signals,
+}: {
+  message: Message;
+  signals: Signal[];
+}) {
+  return (
+    <Card
+      key={message.name}
+      className="g-col-12 g-col-sm-6 g-col-md-4 g-col-lg-3 g-col-xxl-2 shade-0 border"
+    >
+      <Card.Body>
+        <Card.Title>{`${message.name} 0x${message.id.toString(
+          16,
+        )}`}</Card.Title>
+        {signals.map((signal) => {
+          return (
+            <Fragment key={signal.name}>
+              {(() => {
+                if (signal.length === 1) {
+                  return oneBitSignal({ message, signal });
+                }
+                if (signal.length === 2) {
+                  return twoBitSignal({ message, signal });
+                }
+                return InputRange({ message, signal });
+              })()}
+            </Fragment>
+          );
+        })}
+      </Card.Body>
+    </Card>
   );
 }
 
@@ -29,67 +129,14 @@ export default function Cards({
 }: {
   messages: Map<string, Message>;
 }) {
-  const oneBitSignal = (signal: Signal) => {
-    return (
-      <span className="d-flex justify-content-between">
-        <Form.Label className="text-wrap w-90">{`${signal.name}`}</Form.Label>
-        <Form.Check type="checkbox" />
-      </span>
-    );
-  };
-  const twoBitSignal = (signal: Signal) => {
-    return (
-      <>
-        <span className="d-flex justify-content-between">
-          <Form.Label className="text-wrap w-100">{`${signal.name}`}</Form.Label>
-        </span>
-        <Form className="d-flex justify-content-between">
-          {[0, 1, 2, 3].map((i) => {
-            return (
-              <Form.Check
-                key={`${signal.name} ${i}`}
-                inline
-                label={`${i}`}
-                name={signal.name}
-                type="radio"
-                id={`inline-radio-${i}`}
-              />
-            );
-          })}
-        </Form>
-      </>
-    );
-  };
   const cards: JSX.Element[] = [];
   messages.forEach((message) => {
-    const signals: JSX.Element[] = [];
+    const signals: Signal[] = [];
     message.signals.forEach((signal) => {
-      signals.push(
-        <Fragment key={signal.name}>
-          {(() => {
-            if (signal.length === 1) {
-              return oneBitSignal(signal);
-            }
-            if (signal.length === 2) {
-              return twoBitSignal(signal);
-            }
-            return InputRange({ signal });
-          })()}
-        </Fragment>,
-      );
+      signals.push(signal);
     });
     cards.push(
-      <Card
-        key={message.name}
-        className="g-col-12 g-col-sm-6 g-col-md-4 g-col-lg-3 g-col-xxl-2 shade-0 border-0"
-      >
-        <Card.Body>
-          <Card.Title>{`${message.name} 0x${message.id.toString(
-            16,
-          )}`}</Card.Title>
-          {signals}
-        </Card.Body>
-      </Card>,
+      <SignalsCard key={message.name} message={message} signals={signals} />,
     );
   });
   return cards;
